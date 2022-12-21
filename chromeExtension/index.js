b96426511b68b6817a634b1e84af10107bf595f9
kopiHyper <oktawiankopica2001@gmail.com>
2022-12-21 22:17:15 +0100

    Added function that show inputed links in <ul>

--- chromeExtension/index.js
+++ chromeExtension/index.js
@@ -1,9 +1,23 @@
 const inputBtn = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el");
+const ulEl = document.getElementById("u-list");
 
 let myLeads = [];
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
-  console.log(myLeads);
+  inputEl.value = "";
+  renderLeads();
 });
+
+const renderLeads = () => {
+  let listItem = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    listItem += `
+    <li>
+        <a target='_blank' href='${myLeads[i]}'> ${myLeads[i]} </a>
+    </li>`;
+  }
+
+  ulEl.innerHTML = listItem;
+};

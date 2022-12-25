91c545c67e83a54101145ddc0b05ed910f40cd57
kopiHyper <oktawiankopica2001@gmail.com>
2022-12-25 15:44:23 +0100

    Added delete button and cleaned code

--- chromeExtension/index.js
+++ chromeExtension/index.js
@@ -1,21 +1,38 @@
 const inputBtn = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("u-list");
+const delBtn = document.getElementById("delete-btn");
 
 let myLeads = [];
 
+//Getting elements form localStorage
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
+//Assigning them to myLeads array and rendering them
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  renderLeads();
+}
+
+//Saving elements in localStorage form the input element
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = "";
 
-  localStorage.setItem("myLead", JSON.stringify(myLeads));
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
 
   renderLeads();
-
-  console.log(localStorage.getItem("myLead"));
 });
 
-const renderLeads = () => {
+//Deleting all elements on double click
+delBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLeads = [];
+  renderLeads();
+});
+
+//->Rendering elements in DOM
+function renderLeads() {
   let listItem = "";
   for (let i = 0; i < myLeads.length; i++) {
     listItem += `
@@ -25,4 +42,5 @@ const renderLeads = () => {
   }
 
   ulEl.innerHTML = listItem;
-};
+}
+//<-
